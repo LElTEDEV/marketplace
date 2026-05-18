be5009fd50b0d4e5878254fdd153631f0b166f9d
LElTEDEV <zinenizi@gmail.com>
2026-05-18 11:31:02 -0300

    feat: adicionado o redirect para a tela de login

--- marketplace-mobile/src/app/index.tsx
+++ marketplace-mobile/src/app/index.tsx
@@ -1,14 +1,5 @@
-import { router } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Redirect } from "expo-router";
 
 export default function App() {
-  return (
-    <View>
-      <Text>Olá Mundo!!</Text>
-
-      <TouchableOpacity onPress={() => router.push("/login")}>
-        <Text>Login</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  return <Redirect href={"/login"} />;
 }
