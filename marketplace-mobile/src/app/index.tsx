bfdf2beb1c3beb931da9ccdd2a25d8421a9a8f6e
LElTEDEV <zinenizi@gmail.com>
2026-05-18 10:17:22 -0300

    feat: adicionado nativewind no projeto

---
+++ marketplace-mobile/src/app/index.tsx
@@ -0,0 +1,14 @@
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Olá Mundo!!</Text>
+
+      <TouchableOpacity onPress={() => router.push("/login")}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
