05af32bcfc93c6243480efe35cfa801600f0c320
LElTEDEV <zinenizi@gmail.com>
2026-05-18 14:08:24 -0300

    feat: criado logica simples para rota privada e publica

--- marketplace-mobile/src/app/index.tsx
+++ marketplace-mobile/src/app/index.tsx
@@ -1,5 +1,12 @@
 import { Redirect } from "expo-router";
 
 export default function App() {
+  const userData = {
+    token: "wuigjgwigwgwk",
+    name: "Igor",
+  };
+
+  if (userData) return <Redirect href={"/(private)/home"} />;
+
   return <Redirect href={"/login"} />;
 }
