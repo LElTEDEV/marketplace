80604e6bffdfdf877e3745230eb4230f4560c11a
LElTEDEV <zinenizi@gmail.com>
2026-05-19 10:13:03 -0300

    feat: criado controller com useForm para registro

--- marketplace-mobile/src/viewModels/Register/useRegister.viewModel.ts
+++ marketplace-mobile/src/viewModels/Register/useRegister.viewModel.ts
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { RegisterFormData, registerScheme } from "./register.scheme";
 
 export const useRegisterViewModel = () => {
-  const [userData, setUserData] = useState({
-    name: "Igor",
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>({
+    resolver: yupResolver(registerScheme),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      phone: "",
+    },
   });
 
-  return { userData, setUserData };
+  const onSubmit = handleSubmit(({}) => {});
+
+  return { control, errors, onSubmit };
 };
