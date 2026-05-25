ca924e2fd3f753e157534b05b99daf21394f2e0e
LElTEDEV <zinenizi@gmail.com>
2026-05-25 10:25:01 -0300

    feat: criado botao para voltar a tela de login

--- marketplace-mobile/src/viewModels/Register/Register.view.tsx
+++ marketplace-mobile/src/viewModels/Register/Register.view.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
+import { router } from "expo-router";
 import { View, Text, TouchableOpacity } from "react-native";
 
 import { useRegisterViewModel } from "./useRegister.viewModel";
+import { AuthFormHeader } from "../../shared/components/AuthFormHeader";
 import { AppInputController } from "../../shared/components/AppInputController";
 
 export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
@@ -11,6 +13,11 @@ export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
 }) => {
   return (
     <View className="flex-1 items-center justify-center">
+      <AuthFormHeader
+        title="Crie sua conta"
+        subTitle="Informe os seus dados pessoais e de acesso"
+      />
+
       <AppInputController
         label="E-MAIL"
         leftIcon="mail-outline"
@@ -20,6 +27,10 @@ export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
 
       <TouchableOpacity onPress={onSubmit}>
         <Text>Registrar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => router.push("/login")}>
+        <Text>Login</Text>
       </TouchableOpacity>
     </View>
   );
