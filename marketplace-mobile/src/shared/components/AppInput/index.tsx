7f976b5b3e0f7e0e4fa9da567f0d2e678ba9024a
LElTEDEV <zinenizi@gmail.com>
2026-05-25 08:14:37 -0300

    feat: iniciando o appinput

---
+++ marketplace-mobile/src/shared/components/AppInput/index.tsx
@@ -0,0 +1,85 @@
+import {
+  Pressable,
+  Text,
+  TextInput,
+  TextInputProps,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { FC } from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+import { appInputVariants, AppInputVariantsProps } from "./input.variants";
+import { useAppInputViewModel } from "./useAppInputViewModel";
+
+export interface AppInputProps extends TextInputProps, AppInputVariantsProps {
+  label?: string;
+  leftIcon?: keyof typeof Ionicons.glyphMap;
+  rightIcon?: keyof typeof Ionicons.glyphMap;
+  containerClassName?: string;
+  mask?: (value: string) => void | string;
+  error?: string;
+}
+
+export const AppInput: FC<AppInputProps> = ({
+  label,
+  leftIcon,
+  rightIcon,
+  containerClassName,
+  value,
+  isError,
+  secureTextEntry,
+  onBlur,
+  onFocus,
+  onChangeText,
+  mask,
+  error,
+  isDisabled,
+  ...textInputProps
+}) => {
+  const {
+    getIconColor,
+    handleBlur,
+    handleFocus,
+    handlePasswordToggle,
+    handleWrapperPress,
+    showPassword,
+    handleTextChange,
+    isFocused,
+  } = useAppInputViewModel({
+    error,
+    onBlur,
+    onFocus,
+    isError: !!error,
+    mask,
+    onChangeText,
+    isDisabled,
+    secureTextEntry,
+    value,
+  });
+
+  const styles = appInputVariants({
+    isFocused,
+  });
+
+  return (
+    <View className={styles.container({ className: containerClassName })}>
+      <Text className={styles.label()}>{label}</Text>
+
+      <Pressable className={styles.wrapper()}>
+        <Ionicons className="mr-3" size={22} name="person" />
+
+        <TextInput
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          className={styles.input()}
+          {...textInputProps}
+        />
+
+        <TouchableOpacity>
+          <Ionicons size={22} name="eye-off-outline" />
+        </TouchableOpacity>
+      </Pressable>
+    </View>
+  );
+};
---
+++ marketplace-mobile/src/shared/components/AppInput/useAppInputViewModel.ts
@@ -0,0 +1,77 @@
+import { useRef, useState } from "react";
+import { TextInput, FocusEvent, BlurEvent } from "react-native";
+import { colors } from "../../../styles/colors";
+
+interface AppInputViewModelProps {
+  isError?: boolean;
+  isDisabled?: boolean;
+  error?: string;
+  secureTextEntry?: boolean;
+  onFocus?: (event: FocusEvent) => void;
+  onBlur?: (event: BlurEvent) => void;
+  mask?: (text: string) => string | void;
+  onChangeText?: (text: string) => string | void;
+  value?: string;
+}
+
+export const useAppInputViewModel = ({
+  error,
+  isDisabled,
+  isError,
+  mask,
+  onBlur,
+  onChangeText,
+  onFocus,
+  secureTextEntry,
+  value,
+}: AppInputViewModelProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const inputRef = useRef<TextInput>(null);
+
+  const handlePasswordToggle = () => {
+    setShowPassword((prevValue) => !prevValue);
+  };
+
+  const handleWrapperPress = () => {
+    inputRef.current?.focus();
+  };
+
+  const handleFocus = (event: FocusEvent) => {
+    setIsFocused(true);
+    onFocus?.(event);
+  };
+
+  const handleBlur = (event: BlurEvent) => {
+    setIsFocused(false);
+    onBlur?.(event);
+  };
+
+  const getIconColor = () => {
+    if (isFocused) return colors["purple-base"];
+    if (isError) return colors.danger;
+    if (value) return colors["purple-base"];
+
+    return colors.gray[200];
+  };
+
+  const handleTextChange = (text: string) => {
+    if (mask) {
+      onChangeText?.(mask(text) || "");
+    } else {
+      onChangeText?.(text);
+    }
+  };
+
+  return {
+    getIconColor,
+    handleBlur,
+    handleFocus,
+    handleWrapperPress,
+    handlePasswordToggle,
+    showPassword,
+    handleTextChange,
+    isFocused,
+  };
+};
